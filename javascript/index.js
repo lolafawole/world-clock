fdedfe3d5b168c922c58f8f3ad4ee1348d3758df
lolafawole <30295529+lolafawole@users.noreply.github.com>
2025-12-12 18:29:11 +0200

    adding locations logic

--- javascript/index.js
+++ javascript/index.js
@@ -24,5 +24,28 @@ function updateTime() {
   lisbonTimeEl.innerHTML = lisbonTime.format("h:mm:s [<small>]A[</small>]");
 }
 
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesEl = document.querySelector("#cities");
+  citiesEl.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:s"
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectEl = document.querySelector("#city");
+citySelectEl.addEventListener("change", updateCity);
